76b770881764b74382695f98e3f5005290fccc36
yarynaburenko <yaryna.burenko@gmail.com>
2024-10-14 15:05:58 +0300

    add implement task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const stateResult = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        newState = { ...newState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        newState = { ...newState };
+
+        for (const key of action.keysToRemove) {
+          delete newState[key];
+        }
+
+        break;
+
+      case 'clear':
+        newState = {};
+        break;
+    }
+    stateResult.push(newState);
+  }
+
+  return stateResult;
 }
 
 module.exports = transformStateWithClones;
